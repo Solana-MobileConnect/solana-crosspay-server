6c922e5b79603a0325c288a26b8995a8daea3b89
Maxim Schmidt <maxim.schmidt@tum.de>
2023-03-24 00:20:28 +0100

    remove tests

--- tests/get_transaction.sh
+++
@@ -1,5 +0,0 @@
-TX_ID=beb9aed6-902d-4202-b81f-b344c17ad792
-
-curl -X POST -d "account=X" http://localhost:3001/get_transaction?transaction_session_id=$TX_ID
-
-#curl -X POST -d "account=X" https://crosspay-server.onrender.com/get_transaction?transaction_session_id=$TX_ID
--- tests/script.sh
+++
@@ -1,1 +0,0 @@
-curl https://crosspay-server.onrender.com/login_session?login_session_id=33693227-2a75-4626-b7fd-6b30212baedd
