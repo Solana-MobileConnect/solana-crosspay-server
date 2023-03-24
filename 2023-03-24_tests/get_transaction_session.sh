bb4407f1fb95495c84eb3fdb0062436912b07265
Maxim Schmidt <maxim.schmidt@tum.de>
2023-03-24 15:08:25 +0100

    tests

---
+++ 2023-03-24_tests/get_transaction_session.sh
@@ -0,0 +1,5 @@
+TX_ID=
+
+curl http://localhost:3001/transaction_session?transaction_session_id=$TX_ID
+
+#curl https://crosspay-server.onrender.com/transaction_session?transaction_session_id=$TX_ID
