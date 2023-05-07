dd76e322b42888a4cb7d3213b47e7802f36b90b5
Maxim Schmidt <maxim.schmidt@tum.de>
2023-05-07 17:51:35 +0200

    user_login error handling

--- routes/user_login.ts
+++ routes/user_login.ts
@@ -24,7 +24,7 @@ type InputData = {
 
 const FUNDED_ACCOUNT_SECRET = process.env.FUNDED_ACCOUNT_SECRET
 
-if(!FUNDED_ACCOUNT_SECRET) throw new Error("No funded account")
+if (!FUNDED_ACCOUNT_SECRET) throw new Error("No funded account")
 
 router.post('/user_login', async (req: Request, res: Response) => {
 
@@ -42,7 +42,8 @@ router.post('/user_login', async (req: Request, res: Response) => {
   }
 
   if (!req.query['login_session_id']) {
-    return res.status(400).send("No session id")
+    res.status(400).send("No session id")
+    return
   }
 
   // TODO: prevent multiple logins
@@ -70,50 +71,53 @@ router.post('/user_login', async (req: Request, res: Response) => {
 
   // Create dummy transaction
 
-  const connection = new Connection(getRPCUrl(new_session.cluster))
+  try {
+    const connection = new Connection(getRPCUrl(new_session.cluster))
 
-  // Options for dummy transactions
-  // no instructions: crashes Phantom
-  // invalid transaction: user gets error message (not acceptable)
-  // self-transfer of account: user may mistakenly sign it and pay tx fees (we accept this), user gets a notification that they received 0 SOL (this is annoying), it's part of the user's tx history now (annoying), issue: user may not have funds (leading to "Can't simulate it" error)
-  // self-transfer of randomly generated account without funds: "Can't simulate it" message on Phantom
-  // Transfer from funded account to itself: works well! (we accept that the user will sign it); users without funds will be excluded, which is normal; Big advantage: the tx associated with the login will not be part of the user's tx history!
+    // Options for dummy transactions
+    // no instructions: crashes Phantom
+    // invalid transaction: user gets error message (not acceptable)
+    // self-transfer of account: user may mistakenly sign it and pay tx fees (we accept this), user gets a notification that they received 0 SOL (this is annoying), it's part of the user's tx history now (annoying), issue: user may not have funds (leading to "Can't simulate it" error)
+    // self-transfer of randomly generated account without funds: "Can't simulate it" message on Phantom
+    // Transfer from funded account to itself: works well! (we accept that the user will sign it); users without funds will be excluded, which is normal; Big advantage: the tx associated with the login will not be part of the user's tx history!
 
-  const transaction = new Transaction().add(
-    SystemProgram.transfer({
-      fromPubkey: fundedPublicKey,
-      toPubkey: fundedPublicKey,
-      lamports: 0
+    const transaction = new Transaction().add(
+      SystemProgram.transfer({
+        fromPubkey: fundedPublicKey,
+        toPubkey: fundedPublicKey,
+        lamports: 0
+      })
+    )
+
+    // If the user approves the transaction, they pay the fee
+    // We should not be the feePayer as this allows for the account to be drained
+    transaction.feePayer = accountPublicKey
+
+    const latestBlockhash = await connection.getLatestBlockhash()
+    transaction.recentBlockhash = latestBlockhash.blockhash
+
+    transaction.sign(fundedKeypair)
+
+    const serializedTransaction = transaction.serialize({
+      requireAllSignatures: false
     })
-  )
-  
-  // If the user approves the transaction, they pay the fee
-  // We should not be the feePayer as this allows for the account to be drained
-  transaction.feePayer = accountPublicKey
+    const encodedTransaction = serializedTransaction.toString('base64')
 
-  const latestBlockhash = await connection.getLatestBlockhash()
-  transaction.recentBlockhash = latestBlockhash.blockhash
+    res.status(200).json({
+      transaction: encodedTransaction,
+      //message: "Ignore this transaction"
+      message: "Logged in!"
+      //message: "Logged in! (Ignore this message)"
+      //message: "Logged in! (Ignore this)"
+      //message: "Ignore this transaction"
+      //message: "Logged in!"
+      //message: "Successfully logged in! (Ignore this transaction)"
+    })
 
-  transaction.sign(fundedKeypair)
-
-  const serializedTransaction = transaction.serialize({
-    requireAllSignatures: false
-  })
-  const encodedTransaction = serializedTransaction.toString('base64')
-
-  res.status(200).json({
-    transaction: encodedTransaction,
-    //message: "Ignore this transaction"
-    message: "Logged in!"
-    //message: "Logged in! (Ignore this message)"
-    //message: "Logged in! (Ignore this)"
-    //message: "Ignore this transaction"
-    //message: "Logged in!"
-    //message: "Successfully logged in! (Ignore this transaction)"
-  })
-
-  return res.status(200)
-
+  } catch (error: any) {
+    console.error(error)
+    res.status(500).json({ message: "Error" })
+  }
 })
 
 export default router
